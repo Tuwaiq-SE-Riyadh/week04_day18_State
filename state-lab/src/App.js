c24394a7faa82239408971023ae3325f8433f3d7
manalalzeer <manalmaz01@gmail.com>
2021-10-26 15:12:18 +0300

    upload the selution

--- state-lab/src/App.js
+++ state-lab/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [Celsius, setCelsius] = useState("");
+  const changeCelsius = (e) => {
+    let result = (((e.target.value * 9)/5) + 32);
+    setCelsius(e.target.value) 
+    setFahrenheit(result)
+    
+  };
+  
+  
+  const [Fahrenheit, setFahrenheit] = useState("");
+  const changeFahrenheit = (e) => {
+    let result = (((e.target.value - 32) * 5) /9 );
+    setFahrenheit(e.target.value)
+    setCelsius(result) 
+  
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <p> Celsius: {Celsius}</p>
+      <p> Fahrenheit: {Fahrenheit}</p>
+
+      <lable>Enter temperature in Celsius: </lable><br/>
+      <input type="text" onChange ={changeCelsius} value={Celsius} />
+      <br/><br/>
+      <lable>Enter temperature in Fahrenheit: </lable><br/>
+      <input type="text" onChange={changeFahrenheit} value={Fahrenheit} />
+
+
+
     </div>
   );
 }
+
+
 
 export default App;
